30284be49a02194ec83ee7e4f3640841c8fe9376
Presiyana Borisova <presiana03@gmail.com>
2025-06-07 21:30:09 +0300

    export added

--- TODO.md
+++ TODO.md
@@ -9,7 +9,5 @@
 5. Import only games that do not exist
 6. Add import for individual room
 7. Add admin panel for editing games
-8. Add export-leaderboard.php in export/
-9. Must automatically import games?
-10. When exporting rooms, their leaderboard is not added - add top 5 on the leaderboard
-11. Leaderboard must be based on room not show all at once
+8. Must automatically import games?
+9.  Leaderboard must be based on room not show all at once
--- export/export-leaderboard.php
+++ export/export-leaderboard.php
@@ -1,47 +1,32 @@
 <?php
-// Exports rooms and games from the DB to /export-data/games.json
+// Exports rooms and leaderboard from the DB to /export-data/games.json
 
 require '../db.php'; 
 
 $outputDir = __DIR__ . '../../export-data';
-$outputFile = $outputDir . '/games.json';
+$outputFile = $outputDir . '/leaderboard.json';
 
 if (!is_dir($outputDir)) {
     mkdir($outputDir, 0777, true);
 }
 
 try {
-    $roomsStmt = $pdo->query("SELECT id, name, steps, time_for_solving FROM rooms");
+    $roomsStmt = $pdo->query("SELECT id, name FROM rooms");
     $rooms = $roomsStmt->fetchAll(PDO::FETCH_ASSOC);
 
     $exportData = ['rooms' => []];
-
-    $gamesStmt = $pdo->prepare("SELECT question, answer, hint FROM games WHERE room_id = :room_id ORDER BY id ASC");
 
     $leaderboardsStmt = $pdo->prepare("SELECT t.name, l.time FROM leaderboard as l 
     JOIN teams as t ON t.id = l.team_id WHERE room_id = :room_id ORDER BY l.time ASC LIMIT 5");
 
     foreach ($rooms as $room) {
-        $gamesStmt->execute([':room_id' => $room['id']]);
-        $games = $gamesStmt->fetchAll(PDO::FETCH_ASSOC);
         $leaderboardsStmt->execute([':room_id' => $room['id']]);
         $leaderboards = $leaderboardsStmt->fetchAll(PDO::FETCH_ASSOC);
 
         $roomObj = [
             'name' => $room['name'],
-            'steps' => (int)$room['steps'],
-            'timeForSolving' => $room['time_for_solving'] ?: "00:00:00",
             'leaderboard' => [], 
-            'games' => []
         ];
-
-        foreach ($games as $game) {
-            $roomObj['games'][] = [
-                'question' => $game['question'],
-                'answer' => $game['answer'],
-                'hint' => $game['hint']
-            ];
-        }
 
         foreach ($leaderboards as $leaderboard) {
             $roomObj['leaderboard'][] = [
